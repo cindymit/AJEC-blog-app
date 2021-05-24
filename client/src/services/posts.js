1f4b8d7cef73693bcde152568bad34ceca44dfff
ealtidor <emanuella.altidor@gmail.com>
2021-05-24 11:53:15 -0400

    Created Form

--- client/src/services/posts.js
+++ client/src/services/posts.js
@@ -2,47 +2,47 @@ import axios from 'axios'
 
 const apiUrl = `https://ajec-blog-app.herokuapp.com/posts`
 
-export const getProducts = async () => {
+export const getPosts = async () => {
   try {
-    const response = await axios(`${apiUrl}/products`)
-    const products = response.data
-    return products
+    const response = await axios(`${apiUrl}/posts`)
+    const posts = response.data
+    return posts
   } catch (error) {
     throw error
   }
 }
 
-export const getProduct = async id => {
+export const getPost = async id => {
   try {
-    const response = await axios(`${apiUrl}/products/${id}`)
-    const product = response.data
-    return product
+    const response = await axios(`${apiUrl}/posts/${id}`)
+    const post = response.data
+    return post
   } catch (error) {
     throw error
   }
 }
 
-export const createProduct = async product => {
+export const createPost = async post => {
   try {
-    const response = await axios.post(`${apiUrl}/products`, product)
+    const response = await axios.post(`${apiUrl}/posts`, post)
     return response.data
   } catch (error) {
     throw error
   }
 }
 
-export const updateProduct = async (id, product) => {
+export const updatePost = async (id, post) => {
   try {
-    const response = await axios.put(`${apiUrl}/products/${id}`, product)
+    const response = await axios.put(`${apiUrl}/posts/${id}`, post)
     return response.data
   } catch (error) {
     throw error
   }
 }
 
-export const deleteProduct = async id => {
+export const deletePost = async id => {
   try {
-    const response = await axios.delete(`${apiUrl}/products/${id}`)
+    const response = await axios.delete(`${apiUrl}/posts/${id}`)
     return response.data
   } catch (error) {
     throw error
