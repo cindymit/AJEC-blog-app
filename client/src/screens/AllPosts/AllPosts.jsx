bd61ddcf175853f68977f4e553d7054e29a645f3
joelgirouxx <j.giroux9@gmail.com>
2021-05-24 12:02:46 -0400

    Post Render

--- client/src/screens/AllPosts/AllPosts.jsx
+++ client/src/screens/AllPosts/AllPosts.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import Layout from '../../components/Layout/Layout';
+import React from "react";
+import Post from "../../components/Post/Post";
+import Layout from "../../components/Layout/Layout";
 
 const AllPosts = () => {
   return (
     <Layout>
-    <div>
-      <h1>This is "All Posts" page!</h1>
+      <div>
+        <h1>This is "All Posts" page!</h1>
+        <Post />
       </div>
     </Layout>
   );
